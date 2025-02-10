2711a44e9abcc6d0528abb3e9696a6797c90ac91
JD-354 <panamenoarboleda@gmail.com>
2025-02-09 21:41:29 -0500

    Eliminando archivos del repositorio remoto

--- backend/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser,AuthUser } from "../controllers/ctrlUser";
-
-const router = Router();
-
-router.post("/",createUser);
-router.post("/auth",AuthUser);
-
-export default router;
